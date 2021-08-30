bd12de6d89f2f8baee899d10cf2ef56f90f22bd0
Orient1994 <51028785+Orient1994@users.noreply.github.com>
2021-08-30 18:51:41 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,8 +2,7 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo for Webhook Demo...\n")
 print("Jenkins Second Jobs\n")
-
-
+print("Demo for GIThub hook jenkins jobs\n")
 
 
 
